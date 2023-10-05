c3190bddfb05fceee00223adcc51f01aee95ed67
arghyawning <arghya.roy@research.iiit.ac.in>
2023-10-05 12:44:45 +0530

    updated schedulertest to see order of processes

--- initial-xv6/src/user/schedulertest.c
+++ initial-xv6/src/user/schedulertest.c
@@ -18,28 +18,38 @@ int main()
       break;
     if (pid == 0)
     {
+#ifndef FCFS
       if (n < IO)
       {
         sleep(200); // IO bound processes
       }
       else
       {
+#endif
         for (volatile int i = 0; i < 1000000000; i++)
         {
         } // CPU bound process
+#ifndef FCFS
       }
-      // printf("Process %d finished\n", n);
+#endif
+      printf("\nProcess %d finished", n);
       exit(0);
+    }
+    else
+    {
+#ifdef PBS
+      set_priority(60 - IO + n, pid); // Will only matter for PBS, set lower priority for IO bound processes
+#endif
     }
   }
   for (; n > 0; n--)
   {
-    if (waitx(0, &wtime, &rtime) >= 0)
+    if (waitx(0, &wtime, &rtime) >= 0) // fixed :)
     {
       trtime += rtime;
       twtime += wtime;
     }
   }
-  printf("Average rtime %d,  wtime %d\n", trtime / NFORK, twtime / NFORK);
+  printf("\nAverage rtime %d,  wtime %d\n", trtime / NFORK, twtime / NFORK);
   exit(0);
 }
